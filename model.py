650b02168ca599515b04f78ecb49225f359c3e4b
ddl-subir-m <117772994+ddl-subir-m@users.noreply.github.com>
2023-09-16 00:00:33 -0700

    Update model.py

--- model.py
+++ model.py
@@ -15,17 +15,27 @@ os.environ['LD_LIBRARY_PATH'] =  cuda_install_dir
      
 # Load the ctranslate model
 model_device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model_path = '/mnt/artifacts/llama2/final_merged_checkpoint/'
+# model_path = '/mnt/artifacts/llama2/final_merged_checkpoint/'
 
-generator = AutoModelForCausalLM.from_pretrained(model_path,
-    low_cpu_mem_usage=True,
-    return_dict=True,
-    cache_dir="/mnt/artifacts/llama2-model-cache/",
-    torch_dtype=torch.float16,
-    device_map='auto',
-)
-# load the tokenizer
-tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
+model_path = '/mnt/data/llama2-ct'
+model_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# load the ctranslate model
+generator = ctranslate2.Generator(model_path, device=model_device)
+tokenizer = transformers.AutoTokenizer.from_pretrained('subirmansukhani/llama-2-7b-miniguanaco')
+
+pct_new_tokens = 1.2
+
+
+# generator = AutoModelForCausalLM.from_pretrained(model_path,
+#     low_cpu_mem_usage=True,
+#     return_dict=True,
+#     cache_dir="/mnt/artifacts/llama2-model-cache/",
+#     torch_dtype=torch.float16,
+#     device_map='auto',
+# )
+# # load the tokenizer
+# tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
 
 prompt_template = f"<s>[INST] {{dialogue}} [/INST]"
      
@@ -37,22 +47,15 @@ def generate(prompt: str = None, new_tokens: int = 200):
     # Construct the prompt for the model
     user_input = prompt_template.format(dialogue=prompt)
     
+    tokens = tokenizer.convert_ids_to_tokens(tokenizer.encode(user_input))
+    input_length = len(tokens)
+    new_tokens = round(pct_new_tokens*input_length)
     tokens_per_sec = 0
-    start_time = time.perf_counter()
-    input_ids = tokenizer(user_input, return_tensors="pt").input_ids
-    input_ids = input_ids.to('cuda')
-
-    generation_config = GenerationConfig(
-            pad_token_id=tokenizer.pad_token_id,
-            max_new_tokens = new_tokens
-        )
-
-    with torch.no_grad():
-        generated_ids = generator.generate(input_ids, generation_config=generation_config)
-    
-    gen_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-    end_time = time.perf_counter()
-    gen_text = gen_text.replace(f"[INST] {prompt} [/INST]", '')
+    start_time = time.time()
+    results = generator.generate_batch([tokens], sampling_topk=10, max_length=new_tokens, include_prompt_in_result=False)
+    end_time = time.time()
+    output_text = tokenizer.decode(results[0].sequences_ids[0])
     tokens_per_sec = round(new_tokens / (end_time - start_time),3)
+    
     return {'text_from_llm': gen_text, 'tokens_per_sec': tokens_per_sec}
     
