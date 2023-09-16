07f4bcad0ebdd9a3db865820e7776a8fcd6e3206
ddl-subir-m <117772994+ddl-subir-m@users.noreply.github.com>
2023-09-16 00:22:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ os.environ['LD_LIBRARY_PATH'] =  cuda_install_dir
 
 
 # Load the model
-model_path = '/mnt/data/llama2-ct'
+model_path = '/mnt/artifacts/llama2-ct'
 model_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # load the ctranslate model
